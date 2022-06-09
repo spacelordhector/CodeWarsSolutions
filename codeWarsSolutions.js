56a780195dee5af02f0b1db4c8d9543292ab9818
Hector Torres <h.s.torres96@gmail.com>
2022-06-09 16:50:24 -0700

    9 June update

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -247,3 +247,16 @@ function squareOrSquareRoot(array) {
     })
     return newArray
 }
+//TITLE: REMOVING ELEMENTS
+//Take an array and remove every second element from the array.
+// Always keep the first element and start removing with the next element.
+//None of the arrays will be empty, so you don't have to worry about that!
+function removeEveryOther(arr) {
+    let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray
+}
