ea708f7bc9cf657efa40168bb903b323903e6139
Hector Torres <h.s.torres96@gmail.com>
2022-06-11 19:35:52 -0700

    11 June update

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -260,3 +260,14 @@ function removeEveryOther(arr) {
     }
     return newArray
 }
+//TITLE: REMOVE FIRST AND LAST CHARACTER
+//Your goal is to create a function that removes the first and last characters of a string. 
+//You're given one parameter, the original string. 
+//You don't have to worry with strings with less than two characters.
+function removeChar(str) {
+    let newStr = ''
+    for (let i = 1; i < str.length - 1; i++) {
+        newStr += str[i]
+    }
+    return newStr
+};
