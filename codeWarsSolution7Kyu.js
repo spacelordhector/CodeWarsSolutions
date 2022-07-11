5406755a2e5051021883397eca949f769423d9b3
Hector Torres <h.s.torres96@gmail.com>
2022-07-11 15:15:25 -0700

    11 July 2022 update

--- codeWarsSolution7Kyu.js
+++ codeWarsSolution7Kyu.js
@@ -83,3 +83,15 @@ const sequenceSum = (begin, end, step) => {
     }
     return total
 };
+
+//TITTLE: STRING ENDS WITH?
+//Complete the solution so that it returns
+// true if the first argument(string) passed in ends with the 2nd argument (also a string).
+function solution(str, ending) {
+    let endingLength = ending.length
+    if (str.slice(-endingLength) === ending || ending === '') {
+        return true
+    } else {
+        return false
+    }
+}
